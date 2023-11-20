59326e509981f8bd124f80e4921ae3116e323d99
ioeides <ioeides>
2023-11-20 19:50:35 +0100

    added sensors and objects

--- convert-to-png.sh
+++ convert-to-png.sh
@@ -6,3 +6,25 @@ do
 	f_basename=$(basename "$f" .svg)
 	inkscape -w 256 -h 256 $f -o buttons-png/"$f_basename".png
 done
+
+
+
+rm -rf objects-png
+mkdir -p objects-png
+
+for f in objects/*.svg
+do
+	f_basename=$(basename "$f" .svg)
+	inkscape -w 256 -h 256 $f -o objects-png/"$f_basename".png
+done
+
+
+
+rm -rf sensors-png
+mkdir -p sensors-png
+
+for f in sensors/*.svg
+do
+	f_basename=$(basename "$f" .svg)
+	inkscape -w 256 -h 256 $f -o sensors-png/"$f_basename".png
+done
